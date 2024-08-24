01e954d104bff9487a0d4544d2ccada44fafb1ce
Luca Belluso <lucabelluso123@gmail.com>
2024-08-25 01:42:35 +0200

    Deleted imported templates code

---
+++ Source/MovementFPS/Public/BasePlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BasePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVEMENTFPS_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
